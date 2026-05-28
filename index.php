3de875f17cd5269d2e14110f26f5006a2a16c9a1
Julian Meñaca <jsmenaca@gmail.com>
2026-05-28 07:15:19 -0500

    cambio de nombre del semaforo

--- index.php
+++ index.php
@@ -24,7 +24,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Semaforo</title>
+    <title>Semaforo de php con html, boostrap y css</title>
     <meta http-equiv = "refresh" content = "<?= $segundosHastaCambio ?>">
     <style> 
          body {
