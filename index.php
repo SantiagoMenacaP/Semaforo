a0ea7692a4ac36c822a8a3078ef9a20187915ed2
Julian Meñaca <jsmenaca@gmail.com>
2026-05-28 07:25:17 -0500

    mostrar más cambios

--- index.php
+++ index.php
@@ -24,7 +24,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Semaforo de php con html, boostrap y css</title>
+    <title>verificar cambios</title>
     <meta http-equiv = "refresh" content = "<?= $segundosHastaCambio ?>">
     <style> 
          body {
@@ -34,7 +34,7 @@
             align-items: center;
             min-height: 100vh;
             margin: 0;
-            background: #e8e8e8;
+            background: #e8e8e4;
             font-family: system-ui, sans-serif;
         }
         .semaforo{
